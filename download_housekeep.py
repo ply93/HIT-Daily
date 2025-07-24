d504accd85ebf365890e0358e8f6917607d204c0
ply93 <paklun0604@gmail.com>
2025-07-24 10:43:01 +0800

    Update download_housekeep.py

--- download_housekeep.py
+++ download_housekeep.py
@@ -4,11 +4,11 @@ from datetime import datetime
 import pytz
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import TimeoutException, NoSuchElementException
+from selenium.common.exceptions import TimeoutException
+from webdriver_manager.chrome import ChromeDriverManager
 
 # 確保環境準備
 def setup_environment():
