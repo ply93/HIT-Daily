03413fb34541f8783a02865cc43b8cdc071322d7
ply93 <paklun0604@gmail.com>
2025-07-24 10:13:12 +0800

    Update download_cplus.py

--- download_cplus.py
+++ download_cplus.py
@@ -409,7 +409,7 @@ if __name__ == "__main__":
             smtp_port = 587
             sender_email = os.environ.get('ZOHO_EMAIL', 'paklun_ckline@zohomail.com')
             sender_password = os.environ.get('ZOHO_PASSWORD', '@d6G.Pie5UkEPqm')
-            receiver_email = 'paklun@ckline.com.hk'
+            receiver_email = 'ckeqc@ckline.com.hk'
 
             # 創建郵件
             msg = MIMEMultipart()
