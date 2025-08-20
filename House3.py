06a98ab50583cbfec22908e21d92ea143f2493c2
ply93 <paklun0604@gmail.com>
2025-08-20 17:16:29 +0800

    Update House3.py

--- House3.py
+++ House3.py
@@ -16,7 +16,6 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.common.exceptions import TimeoutException, ElementClickInterceptedException, NoSuchElementException, WebDriverException
-from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 import logging
 from dotenv import load_dotenv
