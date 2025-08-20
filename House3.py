555c2388bb0ca6f5a53ae0af3f2b405a14d64f3b
ply93 <paklun0604@gmail.com>
2025-08-20 15:48:11 +0800

    Update House3.py

--- House3.py
+++ House3.py
@@ -60,21 +60,12 @@ def get_chrome_options(download_dir):
     chrome_options.add_argument('--headless')
     chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument('--disable-dev-shm-usage')
-    chrome_options.add_argument('--disable-gpu')
-    chrome_options.add_argument('--ignore-certificate-errors')
-    chrome_options.add_argument('--disable-popup-blocking')
-    chrome_options.add_argument('--disable-extensions')
-    chrome_options.add_argument('--no-first-run')
-    chrome_options.add_argument('--window-size=1920,1080')
-    chrome_options.add_argument('--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36')
-    chrome_options.add_argument('--disable-blink-features=AutomationControlled')
     chrome_options.add_experimental_option('excludeSwitches', ['enable-automation'])
-    prefs = {
+    chrome_options.add_experimental_option('prefs', {
         "download.default_directory": download_dir,
         "download.prompt_for_download": False,
         "safebrowsing.enabled": False
-    }
-    chrome_options.add_experimental_option("prefs", prefs)
+    })
     chrome_options.binary_location = '/usr/bin/chromium-browser'
     return chrome_options
 
