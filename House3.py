dc60d949b5b0a3a4fff84a271480bf2a93b374be
ply93 <paklun0604@gmail.com>
2025-08-20 12:09:31 +0800

    Update House3.py

--- House3.py
+++ House3.py
@@ -81,7 +81,7 @@ def get_chrome_options(download_dir):
     }
     chrome_options.add_experimental_option("prefs", prefs)
     chrome_options.binary_location = '/usr/bin/chromium-browser'
-    chrome_options.set_capability('timeouts', {'implicit': 15000, 'pageLoad': 30000, 'script': 10000})
+    chrome_options.set_capability('timeouts', {'implicit': 30000, 'pageLoad': 10000, 'script': 10000})
     return chrome_options
 
 def wait_for_new_file(driver, download_dir, initial_files, expected_filename=None):
