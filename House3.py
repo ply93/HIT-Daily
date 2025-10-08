c290c07fa3ed6162401dcbddd32c5a05370ad8c1
ply93 <paklun0604@gmail.com>
2025-10-08 11:04:24 +0800

    Update House3.py

--- House3.py
+++ House3.py
@@ -56,7 +56,7 @@ def setup_environment():
 
 def get_chrome_options(download_dir):
     chrome_options = Options()
-    chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--headless=new')  # 改成新 headless 模式
     chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument('--disable-dev-shm-usage')
     chrome_options.add_argument('--disable-gpu')
